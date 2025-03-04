e6e6282a8c2484183f704f6912ca447720203d64
nkgxgongxi <nkgxgongxi@gmail.com>
2025-03-04 15:17:39 +1100

    REFACTOR - refine the process to ingest data from API calls to Snowflake tables. added test cases and comments in codebase.

--- financial_analysis_subproject/data_ingestion_utils.py
+++ financial_analysis_subproject/data_ingestion_utils.py
@@ -1,79 +1,113 @@
 import pandas as pd
-import snowflake.connector
+import datetime as dt
+from snowflake.connector import connect
+from snowflake.connector.pandas_tools import write_pandas
 
 from retrieve_config import retrieve_config_info
 
 
+class Snowflake_Ops():
 
-# Read Snowflake configuration information
-config = retrieve_config_info("config.ini")
-snowflake_config = config['snowflake']
-
-
-def get_snowflake_target_table_column_info(df:pd.DataFrame):
-    # Extract column names and data types dynamically from the DataFrame
-    df_columns = df.columns.tolist()  # Get column names
-    dtypes = df.dtypes  # Get data types
-
-    # Map Pandas data types to Snowflake data types
-    type_mapping = {
-        "object": "STRING",
-        "int64": "INT",
-        "float64": "FLOAT",
-        "bool": "BOOLEAN",
-        "datetime64[ns]": "TIMESTAMP"
-    }
-
-    # Generate the CREATE TABLE query dynamically
-    created_table_columns = []
-    for col in df_columns:
-        snowflake_type = type_mapping.get(str(dtypes[col]), "VARCHAR(10000)")  # Default to STRING if type is not mapped
-        created_table_columns.append(f"{col} {snowflake_type}")
-
-    return df_columns, created_table_columns
+    def __init__(self):
+        # Initialise a Snowflake connection using configuration information
+        self.config = None
+        self.conn = None
+        # self.conn = self.establish_snowflake_connection(self.set_snowflake_config())
 
 
-
-def load_data_to_snowflake(df:pd.DataFrame, table_name:str, mode:str, df_columns: list, created_table_columns:list):
-    # Establish a connection to Snowflake
-    conn = snowflake.connector.connect(
-        user=snowflake_config['user'],
-        password=snowflake_config['password'],
-        account=snowflake_config['account'],
-        warehouse=snowflake_config['warehouse'],
-        database=snowflake_config['database'],
-        schema=snowflake_config['schema'],
-        role=snowflake_config['role']
-    )
-
-    # Create a cursor object
-    cursor = conn.cursor()
-
-    # Define the target table name
-    table_name = "news_sources"
-
-    # Create the table in Snowflake (if it doesn't exist)
-    create_table_query = """CREATE TABLE IF NOT EXISTS {table_name} ({table_columns});""".format(table_name=table_name, table_columns=', '.join(created_table_columns))
-    print(create_table_query)
-    cursor.execute(create_table_query)
-
-    delete_records_query = """TRUNCATE TABLE {0}""".format(table_name)
-    if mode == 'overwrite':
-        cursor.execute(delete_records_query)
+    def get_snowflake_config(self):
+        return self.config
     
-    # Insert data from the DataFrame into the Snowflake table
-    for _, row in df.iterrows():
-        insert_query = f"""
-        INSERT INTO {table_name} ({', '.join(df_columns)})
-        VALUES ({', '.join(['%s'] * len(df_columns))});
-        """
-        cursor.execute(insert_query, tuple(row))
+    def set_snowflake_config(self):
+        config_val = retrieve_config_info("config.ini")
+        self.config = config_val['snowflake']
+    
+
+    def establish_snowflake_connection(self, target_database:str = None, target_schema:str = None):
+        self.set_snowflake_config()
+        self.conn = connect(
+            user=self.config['user'],
+            password=self.config['password'],
+            account=self.config['account'],
+            warehouse=self.config['warehouse'],  
+            database=target_database if target_database != None else self.config['database'],
+            schema=target_schema if target_schema != None else self.config['schema'],
+            role=self.config['role']
+        )
+
+    def cleanup_source_data(self, df:pd.DataFrame):
+        df['loaded_by'] = 'system_user'  # Replace with the actual username or system name
+        df['last_updated_at'] = dt.datetime.now()
+        df.columns = [c.upper() for c in df.columns]
+
+        return df
+
+
+
+    def load_data_to_snowflake(self, df:pd.DataFrame, table_name:str, mode:str):
+        # Establish a connection to Snowflake
+        conn = self.conn
+        # Create a cursor object
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(f"SELECT * FROM {table_name} LIMIT 1")
+            table_exists = True
+        except Exception as e:
+            if "does not exist" in str(e):
+                table_exists = False
+            else:
+                raise e
+
+        if not table_exists:
+            # Extract column names and data types dynamically from the DataFrame
+            df_columns = df.columns.tolist()  # Get column names
+            dtypes = df.dtypes  # Get data types
+            # TODO: It seems like most of the data types are object, so they are mapped into String. How can I make this mapping smarter?
+            # print(dtypes)
+            # Map Pandas data types to Snowflake data types
+            type_mapping = {
+                "object": "STRING",
+                "int64": "INT",
+                "float64": "FLOAT",
+                "bool": "BOOLEAN",
+                "datetime64[us]": "TIMESTAMP"
+            }
+
+            # Generate the CREATE TABLE query dynamically
+            table_columns_with_type = []
+            for col in df_columns:
+                snowflake_type = type_mapping.get(str(dtypes[col]), "STRING")  # Default to STRING if type is not mapped
+                table_columns_with_type.append(f"{col.upper()} {snowflake_type}")
+
+            # print(table_columns_with_type)
+
+            create_table_query = """CREATE TABLE IF NOT EXISTS {table_name} ({table_columns});""".format(
+                table_name=table_name, 
+                table_columns=','.join(table_columns_with_type)
+            )
         
-    # Commit the transaction
-    conn.commit()
+            # print(create_table_query)
+            cursor.execute(create_table_query)
 
-    # Close the connection
-    cursor.close()
-    conn.close()
+        
+        if mode == 'overwrite':
+            delete_records_query = """TRUNCATE TABLE {0}""".format(table_name)
+            cursor.execute(delete_records_query)
+        
+        df['LAST_UPDATED_AT'] = df['LAST_UPDATED_AT'].dt.strftime('%Y-%m-%dT%H:%M:%S')
+        # Load the DataFrame into Snowflake
+        success, nchunks, nrows, _ = write_pandas(
+            conn=conn,
+            df=df,
+            table_name=table_name
+        )
 
-    print("Data successfully loaded into Snowflake!")
+        if success:
+            print(f"Data loaded successfully: {nrows} rows inserted into {table_name}.")
+        else:
+            print("Failed to load data.")
+
+    
+    def close_snowflake_connection(self):
+        self.conn.close()
--- financial_analysis_subproject/news_api_test.py
+++ financial_analysis_subproject/news_api_test.py
@@ -2,7 +2,7 @@ from newsapi import NewsApiClient
 import configparser
 import os
 import pandas as pd
-from data_ingestion_utils import load_data_to_snowflake, get_snowflake_target_table_column_info
+from data_ingestion_utils import Snowflake_Ops
 from retrieve_config import retrieve_config_info
 
 
@@ -20,8 +20,22 @@ newsapi = NewsApiClient(api_key=my_api_key)
 #                                           sources='bbc-news',
 #                                           language='en')
 
-# /v2/everything
 def get_news_data(sources:list, start_date:str, end_date:str, topic:str):
+    """
+        This functions calls NesAPI to retrieve news details based on given search criteria. 
+
+        keyword arguments:
+
+            - sources: a list of string with the news sources user would like to search. In this project, user can get retrieve this reference data from a Snowflake table. 
+            - start_date & end_date: strings format YYYY-MM-DD to define the search date range.
+            - topic: a string to define the topic a user would like to search.
+
+        Returns:
+
+            If there is news suitable for searching criteria, data will be returned in python dict format; Otherwise, returning None value. 
+        
+    """
+    # Validate News Source Info
     if len(sources) == 0:
         print("No source is provided... no data is retrieved.")
         return None
@@ -35,14 +49,22 @@ def get_news_data(sources:list, start_date:str, end_date:str, topic:str):
     
     all_articles = newsapi.get_everything(q=topic,
                                             sources=source_str,
-                                            from_param='2025-02-11',
-                                            to='2025-03-01',
+                                            from_param=start_date,
+                                            to=end_date,
                                             language='en',
                                             sort_by='relevancy',
                                             page=1)
     
-    print(all_articles)
-    return None
+    # Handle the situation where there is no data found.
+    number_of_news = all_articles['totalResults']
+    print("{number_of_news} news found from provided sources with related topic.".format(number_of_news=number_of_news))
+    if  number_of_news > 0:    
+        news_data = all_articles['articles']
+    else:
+        return None
+    
+    # TODO: How to get data with pagination (second page or so).
+    return news_data
     
 
 # /v2/top-headlines/sources
@@ -58,21 +80,40 @@ def get_news_sources_data():
 
     return df
 
+def ingest_news_sources(snowflake_ops:Snowflake_Ops):
+    source_df = get_news_sources_data()
+    print("There are {0} sources from NewsAPI.".format(source_df.shape[0]))
+    source_df = snowflake_ops.cleanup_source_data(source_df)
+    snowflake_ops.load_data_to_snowflake(source_df, 
+                           table_name="NEWS_SOURCES", 
+                           mode="overwrite"
+                           )
+    
+def ingest_news_data(snowflake_ops:Snowflake_Ops):
+    news_data = get_news_data(sources=['bloomberg', 'the-wall-street-journal', 'techcrunch', 'fortune', 'the-next-web', 'cnn', 'abc-news', 'google-news'], topic='AI', start_date='2025-02-25', end_date='2025-03-03')
+    if news_data != None: 
+        print(len(news_data))
+        news_df = pd.DataFrame(news_data)
+        print(news_df.columns)
+        news_df[['source_id', 'source_name']] = pd.json_normalize(news_df['source'].values.tolist())
+        news_df['topic'] = 'AI'
+        news_df = news_df[['title', 'content', 'author', 'url', 'source_id', 'topic', 'publishedAt']]
+
+        news_df = snowflake_ops.cleanup_source_data(news_df)
+        snowflake_ops.load_data_to_snowflake(news_df, 
+                           table_name="NEWS", 
+                           mode="overwrite"
+                           )
+
 
 
 if __name__ == '__main__':
-    # source_df = get_news_sources_data()
-    # print("There are {0} sources from NewsAPI.".format(source_df.shape[0]))
-    # df_columns, table_columns = get_snowflake_target_table_column_info(source_df)
-    # print(table_columns)
-    # load_data_to_snowflake(source_df, 
-    #                        table_name="NEWS_SOURCES", 
-    #                        mode="overwrite", 
-    #                        df_columns=df_columns,
-    #                        created_table_columns=table_columns
-    #                        )
-
-    get_news_data(sources=['bloomberg'], topic='trump', start_date='', end_date='')
+    # print(get_news_data.__doc__)
+    testSnowOps = Snowflake_Ops()
+    testSnowOps.establish_snowflake_connection(target_database='RAW', target_schema='FINANCIAL_INFO')
+    # ingest_news_sources(testSnowOps)
+    ingest_news_data(testSnowOps)
+    testSnowOps.close_snowflake_connection()
 
 
 
