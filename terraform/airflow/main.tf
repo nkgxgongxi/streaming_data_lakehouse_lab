80b74aa92f41c407767baab8a0be4f8d1c37bf1c
nkgxgongxi <nkgxgongxi@gmail.com>
2025-03-05 15:27:20 +1100

    DEBUG - correct shell commands to make Airflow server tailored.

--- terraform/airflow/main.tf
+++ terraform/airflow/main.tf
@@ -8,7 +8,7 @@ data "aws_subnet" "existing" {
 
 resource "aws_instance" "airflow" {
   ami                    = "ami-064b71eca68aadfb8"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.medium"
   subnet_id              = data.aws_subnet.existing.id
   vpc_security_group_ids = [var.security_group_id]
   key_name               = "kafka-ec2-key"
@@ -21,19 +21,30 @@ resource "aws_instance" "airflow" {
               #!/bin/bash
               sudo yum update -y
               sudo yum install -y python3-pip
-              sudo mkdir -p /opt/airflow_dir
-              sudo chown -R ec2-user:ec2-user /opt/airflow_dir
-              cd /opt/airflow_dir
+              sudo mkdir -p /opt/airflow_project
+              sudo chown -R ec2-user:ec2-user /opt/airflow_project
+              cd /opt/airflow_project
               python3 -m venv airflow-venv
               source airflow-venv/bin/activate
+              
               pip install apache-airflow[celery,postgres]
+              pip install pandas
+
+              sudo mkdir -p /opt/airflow_home
+              sudo chown -R ec2-user:ec2-user /opt/airflow_home
+
+              echo 'export AIRFLOW_HOME=/opt/airflow_home' >> ~/.bashrc
+              source ~/.bashrc
+
               airflow db init
               airflow users create --username admin --password admin --firstname Admin --lastname User --role Admin --email nkgxgongxi@gmail.com
+
+              sed -i 's/^load_examples = True/load_examples = False/' /opt/airflow_home/airflow.cfg
               airflow webserver -D
               airflow scheduler -D
               EOF
-
-  tags = {
+  
+   tags = {
     Name = "Airflow-Server"
   }
 }
