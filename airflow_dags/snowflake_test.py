c625561f901bb8ab49703ee2bc367fc06529b316
nkgxgongxi <nkgxgongxi@gmail.com>
2025-03-07 23:15:31 +1100

    NEW - Test Snowflake connection

---
+++ airflow_dags/snowflake_test.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 3, 7),
+    'retries': 1,
+}
+
+with DAG(
+    'snowflake_operator_dag',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+
+    run_query = SnowflakeOperator(
+        task_id='run_query',
+        sql='SELECT COUNT(*) FROM NEWS',
+        snowflake_conn_id='snowflake-test-conn',
+    )
+
+    run_query
