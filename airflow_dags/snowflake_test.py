270e521e33190b65491cd60e8f572891f48457b1
nkgxgongxi <nkgxgongxi@gmail.com>
2025-03-10 10:25:30 +1100

    NEW FEATURE - change snowflake connection to refer to snowflake-python-connector.

--- airflow_dags/snowflake_test.py
+++ airflow_dags/snowflake_test.py
@@ -4,6 +4,10 @@ from utils.data_ingestion_utils import Snowflake_Ops
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
+test_snowflake_ops = Snowflake_Ops()
+test_snowflake_ops.config_location = '/opt/airflow_home'
+test_snowflake_ops.establish_snowflake_connection(target_database='RAW', target_schema='FINANCIAL_INFO')
+
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2025, 3, 7),
@@ -20,7 +24,7 @@ with DAG(
     ingest_news_sources_task = PythonOperator(
         task_id='Ingest_News_Source',
         python_callable = ingest_news_sources,
-        op_kwargs={'snowflake_ops':Snowflake_Ops(config_location='/opt/airflow_home/')}
+        op_kwargs={'snowflake_ops':test_snowflake_ops}
     )
 
     ingest_news_sources_task
